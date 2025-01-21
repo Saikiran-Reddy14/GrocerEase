806ba689b854fdb1aa580d30378845cf7e2b17f0
Saikiran-Reddy14 <saikian.kadari14@gmail.com>
2025-01-21 18:35:17 +0530

    Added function to generate and store refresh token for user login

---
+++ backend/utils/generateRefreshToken.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+import UserModel from '../models/userModel';
+
+const generateRefreshToken = async (userId) => {
+  let token = await jwt.sign(
+    { id: userId },
+    process.env.SECRET_KEY_ACCESS_TOKEN,
+    {
+      expiresIn: '7d',
+    }
+  );
+
+  const updateRefreshToken = await UserModel.updateOne(
+    {
+      _id: userId,
+    },
+    { $set: { refresh_token: token } }
+  );
+
+  return token;
+};
+
+export default generateRefreshToken;
