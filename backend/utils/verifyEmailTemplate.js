9c50f2fbe43520768aa286be10b390b0890532cc
Saikiran-Reddy14 <saikian.kadari14@gmail.com>
2025-01-21 16:55:14 +0530

    Modified verify email template

--- backend/utils/verifyEmailTemplate.js
+++ backend/utils/verifyEmailTemplate.js
@@ -4,7 +4,7 @@ const verifyEmailTemplate = ({ name, url }) => {
     <br />
     <br />
     <p>Thank you for registering to GrocerEase</p>
-    <a href="${url}" style="color:white;background:blue;border-radius:20px;margin-top:10px;padding:10px">
+    <a href=${url} style="color:white;background:blue;border-radius:20px;margin-top:10px;padding:10px">
       Verify Email
     </a>
   `;
