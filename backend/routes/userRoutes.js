3132f4cc8219d5b15c1ecee2f6f03429de6f9371
Saikiran-Reddy14 <saikian.kadari14@gmail.com>
2025-01-21 18:04:08 +0530

    Added email verification route to userRouter

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
-import { registerUser } from '../controllers/userController.js';
+import {
+  registerUser,
+  verifyEmailController,
+} from '../controllers/userController.js';
 
 const userRouter = Router();
 
 userRouter.post('/register', registerUser);
+userRouter.post('/verify-email', verifyEmailController);
 
 export default userRouter;
