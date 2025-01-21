dc990e4014012ba77a72fa8efc3a08996de74bab
Saikiran-Reddy14 <saikian.kadari14@gmail.com>
2025-01-21 19:12:18 +0530

    Added logout route to user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,14 +1,17 @@
 import { Router } from 'express';
 import {
   loginUser,
+  logoutUser,
   registerUser,
   verifyEmailController,
 } from '../controllers/userController.js';
+import auth from '../middleware/auth.js';
 
 const userRouter = Router();
 
 userRouter.post('/register', registerUser);
 userRouter.post('/verify-email', verifyEmailController);
 userRouter.post('/login', loginUser);
+userRouter.post('/logout', auth, logoutUser);
 
 export default userRouter;
