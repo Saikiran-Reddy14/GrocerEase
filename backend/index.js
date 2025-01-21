0e73ed6dde53ae1a42512e7e21c03b71105ff9f2
Saikiran-Reddy14 <saikian.kadari14@gmail.com>
2025-01-21 16:11:13 +0530

    Updated user route to use '/api/users' for user-related endpoints

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
 import { connectDB } from './config/connectDB.js';
+import { registerUser } from './controllers/userController.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 const app = express();
@@ -25,8 +27,6 @@ const PORT = process.env.PORT || 8000;
 
 connectDB();
 
-app.get('/', (req, res) => {
-  res.send('Ok');
-});
+app.use('/api/users', userRouter);
 
 app.listen(PORT, () => console.log('Server is running at port', PORT));
